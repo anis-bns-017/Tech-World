75478915a31b6de2ebc1a0e586bfdbb451374bb2
anis-bns-017 <smanisbns@gmail.com>
2024-08-23 21:24:56 +0600

    add product Category and list of them

--- frontend/src/components/CategoryList.jsx
+++ frontend/src/components/CategoryList.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import SummaryApi from "../common";
+import { Link } from "react-router-dom";
 
 const CategoryList = () => {
   const [categoryProduct, setCategoryProduct] = useState([]);
   const [loading, setLoading] = useState(false);
+  const categoryLoading = new Array(13).fill(null);
+
   const fetchCategoryProduct = async () => {
     setLoading(true);
     const response = await fetch(SummaryApi.categoryProduct.url);
@@ -19,20 +22,35 @@ const CategoryList = () => {
   return (
     <div className="container mx-auto p-4">
       <div className="flex items-center gap-4 justify-between overflow-scroll scrollbaar-none">
-        {categoryProduct.map((product, index) => {
-          return (
-            <div key={index} className="cursor-pointer">
-              <div className="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-3 bg-white flex justify-center items-center">
-                <img
-                  src={product.productImage[0]}
-                  alt={product.category}
-                  className="h-full object-fill"
-                />
-              </div>
-              <p className="text-center text-sm md:text-base capitalize">{product.category}</p>
-            </div> 
-          );
-        })}
+        {loading
+          ? categoryLoading.map((el, index) => {
+              return (
+                <div
+                  key={"categoryLoading" + index}
+                  className="h-16 w-16 md:w-20 md:h-20 rounded-full overflow-hidden bg-slate-200 animate-pulse"
+                ></div>
+              );
+            })
+          : categoryProduct.map((product, index) => {
+              return (
+                <Link
+                  to={"/product-category/" + product?.category}
+                  key={product?.category}
+                  className="cursor-pointer"
+                >
+                  <div className="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-4 bg-slate-200 flex justify-center items-center">
+                    <img
+                      src={product.productImage[0]}
+                      alt={product.category}
+                      className="h-full object-scale-down mix-blend-multiply hover:scale-125 transition-all"
+                    />
+                  </div>
+                  <p className="text-center text-sm md:text-base capitalize">
+                    {product?.category}
+                  </p>
+                </Link>
+              );
+            })}
       </div>
     </div>
   );
