5c940fbf97f03dd6551909f6056afb5db0064521
anis-bns-017 <smanisbns@gmail.com>
2024-08-25 13:29:17 +0600

    add slideShare in Banner images

--- frontend/src/components/BannerProduct.jsx
+++ frontend/src/components/BannerProduct.jsx
@@ -1,3 +1,6 @@
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
+
 import image1 from "../assest/banner/img1.webp";
 import image2 from "../assest/banner/img2.webp";
 import image3 from "../assest/banner/img3.jpg";
@@ -9,10 +12,10 @@ import image2Mobile from "../assest/banner/img2_mobile.webp";
 import image3Mobile from "../assest/banner/img3_mobile.jpg";
 import image4Mobile from "../assest/banner/img4_mobile.jpg";
 import image5Mobile from "../assest/banner/img5_mobile.png";
-
-import { FaAngleLeft } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 const BannerProduct = () => {
+  const [currentImage, setCurrentImage] = useState(0);
   const desktopImages = [image1, image2, image3, image4, image5];
   const mobileImages = [
     image1Mobile,
@@ -21,25 +24,87 @@ const BannerProduct = () => {
     image4Mobile,
     image5Mobile,
   ];
-  
+
+  const nextImage = () => {
+    if (desktopImages.length - 1 > currentImage) {
+      setCurrentImage((prev) => prev + 1);
+    }
+  };
+
+  const previousImage = () => {
+    if (currentImage != 0) {
+      setCurrentImage((prev) => prev - 1);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (desktopImages.length - 1 > currentImage) {
+        nextImage();
+      } else {
+        setCurrentImage(0);
+      }
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [currentImage]);
+
   return (
-    <div className="container mt-auto px-4 rounded">
-      <div className="h-72 w-full bg-slate-200">
-        <div className="flex h-full w-full">
-          {desktopImages.map((imageUrl) => {
-            return (
-              <div
-                className="w-full h-full min-w-full min-h-full translate"
-                key={imageUrl}
-                style={{ transform: `translateX(-200%)` }}
+    <>
+      <div className="container mt-auto px-4 rounded">
+        <div className="h-56 w-full bg-slate-200 relative">
+          <div className="absolute z-10 h-full w-full md:flex items-center hidden">
+            <div className="flex justify-between w-full text-2xl">
+              <button
+                onClick={previousImage}
+                className="bg-white shadow-md rounded-full p-1"
               >
-                <img src={imageUrl} alt="" className="w-full h-full" />
-              </div>
-            );
-          })}
+                {<FaAngleLeft />}
+              </button>
+              <button
+                onClick={nextImage}
+                className="bg-white shadow-md rounded-full p-1"
+              >
+                {<FaAngleRight />}
+              </button>
+            </div>
+          </div>
+
+          {/* Desktop and Tablet version */}
+          <div className="hidden md:flex h-full w-full overflow-hidden">
+            {desktopImages.map((imageUrl, index) => {
+              return (
+                <div
+                  className="w-full h-full min-h-full min-w-full transition-all"
+                  key={imageUrl}
+                  style={{ transform: `translateX(-${currentImage * 100}%)` }}
+                >
+                  <img src={imageUrl} alt="" className="w-full h-full" />
+                </div>
+              );
+            })}
+          </div>
+
+          {/* Mobile version */}
+          <div className="flex h-full w-full overflow-hidden md:hidden">
+            {mobileImages.map((imageUrl, index) => {
+              return (
+                <div
+                  className="w-full h-full min-h-full min-w-full transition-all"
+                  key={imageUrl}
+                  style={{ transform: `translateX(-${currentImage * 100}%)` }}
+                >
+                  <img
+                    src={imageUrl}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,12 @@
+import { FaAngleLeft } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <footer className="bg-slate-300">
       <div className="container mx-auto p-4">
-        <p className="text-center font-bold" title = "Youtube Here"> It is a footer </p>
+        <p className="text-center font-bold" title="Youtube Here">
+          It is a footer
+        </p>
       </div>
     </footer>
   );
