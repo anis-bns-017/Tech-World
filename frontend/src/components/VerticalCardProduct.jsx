5782454512cfc2a37494378acd05e85a8a1fa0a1
anis-bns-017 <smanisbns@gmail.com>
2024-08-27 02:03:21 +0600

    Add loading animation on VarticalCardProduct

--- frontend/src/components/VerticalCardProduct.jsx
+++ frontend/src/components/VerticalCardProduct.jsx
@@ -7,7 +7,7 @@ import { FaAngleRight } from "react-icons/fa6";
 
 const VerticalCardProduct = ({ category, heading }) => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const loadingList = new Array(13).fill(null);
   const [scroll, setScroll] = useState(0);
   const scrollElement = useRef();
@@ -54,39 +54,67 @@ const VerticalCardProduct = ({ category, heading }) => {
           {<FaAngleRight />}
         </button>
 
-        {data.map((product, index) => {
-          return (
-            <div
-              key={index}
-              className="w-full max-w-[280px] md: min-w-[340px] bg-white rounded-sm shadow"
-            >
-              <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center">
-                <img
-                  src={product.productImage[0]}
-                  className="object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply"
-                />
-              </div> 
+        {loading
+          ? loadingList.map((product, index) => {
+              return (
+                <div
+                  key={index}
+                  className="w-full max-w-[280px] md: min-w-[340px] bg-white rounded-sm shadow"
+                >
+                  <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse">
+                    {/* <img
+                      src={product.productImage[0]}
+                      className="object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply"
+                    /> */}
+                  </div>
 
-              <div className="p-4 grid gap-3">
-                <h2 className="font-semibold text-base md:text-lg text-ellipsis line-clamp-1 text-black">
-                  {product?.productName}
-                </h2>
-                <p className="capitalize text-slate-500">{product.category}</p>
-                <div className="flex gap-3">
-                  <p className="text-red-600 font-semibold">
-                    {"৳" + displayCurrency(product?.sellingPrice)}
-                  </p>
-                  <p className="text-slate-500 line-through">
-                    {"৳" + displayCurrency(product?.price)}
-                  </p>
+                  <div className="p-4 grid gap-3">
+                    <h2 className="font-semibold text-base md:text-lg text-ellipsis line-clamp-1 text-black  p-1 py-2 animate-pulse rounded-full bg-slate-200"></h2>
+                    <p className="capitalize text-slate-500 p-1 py-2 animate-pulse rounded-full bg-slate-200"></p>
+                    <div className="flex gap-3">
+                      <p className="text-red-600 font-semibold p-1 py-2 animate-pulse rounded-full bg-slate-200 w-full"></p>
+                      <p className="text-slate-500 line-through p-1 py-2 animate-pulse rounded-full bg-slate-200 w-full"></p>
+                    </div>
+                    <button className="text-sm text-white px-3 py-2 rounded-full bg-slate-200 animate-pulse"></button>
+                  </div>
                 </div>
-                <button className="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded-full">
-                  Add to Cart
-                </button>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })
+          : data.map((product, index) => {
+              return (
+                <div
+                  key={index}
+                  className="w-full max-w-[280px] md: min-w-[340px] bg-white rounded-sm shadow"
+                >
+                  <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center">
+                    <img
+                      src={product.productImage[0]}
+                      className="object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply"
+                    />
+                  </div>
+
+                  <div className="p-4 grid gap-3">
+                    <h2 className="font-semibold text-base md:text-lg text-ellipsis line-clamp-1 text-black">
+                      {product?.productName}
+                    </h2>
+                    <p className="capitalize text-slate-500">
+                      {product.category}
+                    </p>
+                    <div className="flex gap-3">
+                      <p className="text-red-600 font-semibold">
+                        {"৳" + displayCurrency(product?.sellingPrice)}
+                      </p>
+                      <p className="text-slate-500 line-through">
+                        {"৳" + displayCurrency(product?.price)}
+                      </p>
+                    </div>
+                    <button className="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded-full">
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
